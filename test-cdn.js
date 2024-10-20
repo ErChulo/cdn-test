9a15d48a40259ba192dc18e2d26af7c81f3c8179
Herick Lopez <herickleninlopezcardona@gmail.com>
2024-10-19 23:23:21 -0400

    test

---
+++ test-cdn.js
@@ -0,0 +1,5 @@
+console.log("If you see this, then it is working.")
+
+document.write("If you see this, then it is working.")
+
+document.getElementById("test-cdn").innerText = "If you see this, then it is fucking working."
